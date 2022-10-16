5dda2ad46fbb65eba406196415ad68a880f38350
Vladimir Khodygo <vladimir.khodygo@glasgow.ac.uk>
2022-10-16 21:11:47 +0100

    MAINT: reformat fastmath class

--- src/main/java/cern/jet/random/engine/FastMathSupport.java
+++ src/main/java/cern/jet/random/engine/FastMathSupport.java
@@ -8,34 +8,33 @@ public class FastMathSupport {
     /**
      * A helper method that calculates the integer part of a {@code log2} of any {@code int} number.
      *
-     * @implSpec Provides valid results when the input is a positive {@code int}.
-     * The exact equivalent of {@code (int) (Math.log(value) / Math.log(2))}, but much faster.
      * @param value An integer number.
      * @return {@code (int) log2(value)}.
+     * @implSpec Provides valid results when the input is a positive {@code int}.
+     * The exact equivalent of {@code (int) (Math.log(value) / Math.log(2))}, but much faster.
      */
-    static int fastLog2(int value)
-    {
+    static int fastLog2(int value) {
         return 31 - Integer.numberOfLeadingZeros(value);
     }
 
     /**
      * A helper method that calculates the integer part of a {@code log2} of any {@code long} number.
      *
+     * @param value An integer number.
+     * @return {@code (int) log2(value)}.
      * @implSpec Provides valid results when the input is a positive {@code long}.
      * The exact equivalent of {@code (long) (Math.log(value) / Math.log(2))}, but much faster, for the range
      * up to {@code Integer.MAX_VALUE} and above. At some point the {@link Math#log(double)} starts to fail to provide
      * the accurate answer due to round-off errors and limited precision.
-     * @param value An integer number.
-     * @return {@code (int) log2(value)}.
      */
-    static long fastLog2 (long value)
-    {
+    static long fastLog2(long value) {
         return 63 - Long.numberOfLeadingZeros(value);
     }
 
     /**
      * Fast division of two integer numbers when the dividend is a non-negative integer number and the divisor is a
      * positive one and a power of {@code 2} as well.
+     *
      * @param a The dividend.
      * @param b The divisor.
      * @return The quotient.
@@ -45,9 +44,9 @@ public class FastMathSupport {
     }
 
     /**
-     * @see #fastDivisionByPowerOfTwo(int, int)
      * @implNote Due to some limitations of floating point math starts to diverge from {@code (int) ((double) a / b)}
      * at some point.
+     * @see #fastDivisionByPowerOfTwo(int, int)
      */
     static long fastDivisionByPowerOfTwo(final long a, final long b) {
         return a >> fastLog2(b);
